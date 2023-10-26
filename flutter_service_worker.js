ad2495fad0623e373586e4a58d4f5f60da343c49
Kakuyo Philip <88200797+philkaks@users.noreply.github.com>
2023-10-26 20:55:45 +0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,8 +23,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "bc455f555feef298f141ae565512a059",
-"/": "bc455f555feef298f141ae565512a059",
+"index.html": "5eb2c52be77faad00f32b7e94ef79759",
+"/": "5eb2c52be77faad00f32b7e94ef79759",
 "main.dart.js": "2655303bcc9af142da7fd0f4d402a80e",
 "manifest.json": "575b9f2920c28889142eeb077c77db82",
 "version.json": "dcd9f72acb5aa39efed3c9d1fc0cd4c7"};
